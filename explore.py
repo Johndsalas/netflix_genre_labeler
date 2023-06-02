e82517cbe12e72621229ee36d984049639f74be8
John Salas <johndsalas@gmail.com>
2023-06-01 19:58:06 -0500

    Finished relative frequency visuals

--- explore.py
+++ explore.py
@@ -17,6 +17,70 @@ def get_gens(df):
 
     return gen_set
 
+def get_majoriety_counts(train):
+    
+    freq_doc = {}
+    freq_count = {}
+
+    list_freq_doc = []
+    list_freq_count = []
+    
+    train_set_of_words = set(train['description'].apply(lambda value : value.split(' '))
+                                                 .explode('description')
+                                                 .to_list())
+    
+    comedy_train = train[train.comedy == True]
+
+    non_comedy_train = train[train.comedy == False]
+    
+    for word in train_set_of_words:
+
+        # get count of comedy films that have word in them
+        comedy_train['com_count'] = comedy_train['description'].apply(lambda words: words.count(word))
+        
+        comedy_train['com_doc'] = comedy_train['com_count'] > 0
+        
+        c_w_count = comedy_train['com_count'].sum()
+        
+        c_d_count = comedy_train['com_doc'].sum()
+        
+        # get count of non-comedy films that have word in them
+        non_comedy_train['non_count'] = non_comedy_train['description'].apply(lambda words: words.count(word))
+        
+        non_comedy_train['non_doc'] = non_comedy_train['non_count'] > 0
+        
+        n_w_count = non_comedy_train['non_count'].sum()
+        
+        n_d_count = non_comedy_train['non_doc'].sum()
+        
+        # subtract number of non-comedy films from number of comedy films
+        
+        w_total = c_w_count - n_w_count
+        
+        d_total = c_d_count - n_d_count
+        
+        # append difference
+        
+        freq_doc[f'{word}'] = d_total
+        
+        list_freq_doc.append(d_total)
+        
+        freq_count[f'{word}'] = w_total
+        
+        list_freq_count.append(w_total)
+        
+    return freq_doc, freq_count, list_freq_doc, list_freq_count
+
+
+
+
+
+
+
+
+
+
+
 
 ####################################### Visualizations ###########################################################
 
@@ -143,4 +207,27 @@ def unique_words_frequency(df, gen_set):
 
     plt.title("On Average Each Show Description Contains Between 2-4 Unique Words")
     plt.tight_layout()
+    plt.show()
+
+
+def omni_pie(panda_series,title = "Super Awsome Title I'll Think of Latter"):
+
+    labels = set(value for value in panda_series)
+
+    values = [len(panda_series[panda_series == label]) for label in labels]
+
+    # generate and show chart
+    plt.pie(values, labels=labels, autopct='%.0f%%', colors=['#ffc3a0', '#c0d6e4', '#cf98eb', '#77d198'])
+    plt.title(f'{title}')
+    plt.show()
+
+
+def get_hist(li):
+    
+    plt.figure(figsize=(20, 5))
+
+    plt.xlim(-50,50)
+
+    plt.hist(li, bins = 500)
+
     plt.show()
